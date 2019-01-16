2378b6d9da23c7f7ebb3d6d470e00709a0cc1c55
qingkong9579 <2597924209@qq.com>
2019-01-16 12:39:06 +0800

    Initial commit

---
+++ app/src/androidTest/java/com/github/qingkong9579/happynewyear/ExampleInstrumentedTest.java
@@ -0,0 +1,26 @@
+package com.github.qingkong9579.happynewyear;
+
+import android.content.Context;
+import android.support.test.InstrumentationRegistry;
+import android.support.test.runner.AndroidJUnit4;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import static org.junit.Assert.*;
+
+/**
+ * Instrumented test, which will execute on an Android device.
+ *
+ * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
+ */
+@RunWith(AndroidJUnit4.class)
+public class ExampleInstrumentedTest {
+    @Test
+    public void useAppContext() {
+        // Context of the app under test.
+        Context appContext = InstrumentationRegistry.getTargetContext();
+
+        assertEquals("com.github.qingkong9579.happynewyear", appContext.getPackageName());
+    }
+}
